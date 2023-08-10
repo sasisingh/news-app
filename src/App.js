be5457135a0db3c79a17a174eaaf435d272c9453
sasisingh <sasisingh6387@gmail.com>
2023-08-11 00:12:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = () => {
     <>
       <div class="container-fluid p-0">
         <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-2 py-lg-0 px-lg-5">
-          <a href="index.html" class="navbar-brand d-block d-lg-none">
+          <a href="#" class="navbar-brand d-block d-lg-none" rel="noreferrer">
             <h1 class="m-0 display-4 text-uppercase text-primary">Biz<span class="text-white font-weight-normal">News</span></h1>
           </a>
           <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
@@ -35,7 +35,7 @@ const App = () => {
           </button>
           <div class="collapse navbar-collapse justify-content-between px-0 px-lg-3" id="navbarCollapse">
             <div class="navbar-nav mr-auto py-0">
-              <a href="index.html" class="nav-item nav-link">Home</a>
+              <a href="#" class="nav-item nav-link" rel="noreferrer">Home</a>
             </div>
           </div>
         </nav>
